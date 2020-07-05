dcdaad29ae22246fe31938c466b2c0537c911091
Renato Alves Leite <renato.sty@gmail.com>
2020-07-05 03:09:41 -0300

    Tentativa de fazer as peças
    tentativa falha de criar classes, erro ao desenhar as peças

--- Arquivos/scripts/main.js
+++ Arquivos/scripts/main.js
@@ -1,15 +1,29 @@
-var canvas, render
+var canvas, render, pieces_img, heart
+/*var w_king, w_queen, b_king, b_queen
+var w_bishops, w_knights, w_rooks, w_pawns
+var b_bishops, b_knights, b_rooks, b_pawns*/
+var w_king = { x: 320, y: 540, img_x: 0, img_y: 0 }
 
 const fps = 1000/30
 
+//configuracao inicial do jogo
 window.onload = function()
 {
     canvas = document.getElementById("mycanvas")
     render = canvas.getContext("2d")
     canvas.width = 640
     canvas.height = 640
+    
+    pieces_img = new Image()
+    pieces_img.src = "img/pieces.png"
 
+    pieces_img.onload = initGame()
+}
+
+function initGame()
+{
     drawBoard()
+    drawPieces()
 }
 
 function drawBoard()
@@ -30,3 +44,48 @@ function drawBoard()
         }
     }
 }
+
+function drawPieces()
+{
+    render.drawImage(pieces_img, 0, 0)
+}
+
+/*var king = function(white, x, y)
+{
+    let img_x = 0
+    let img_y = white ? 0 : 200
+    let pos_x = x
+    let pos_y = y
+}
+
+var queen = function(white, x, y)
+{
+    let img_x = 200
+    let img_y = white ? 0 : 200
+    let pos_x = x
+    let pos_y = y
+}
+
+var bishop = function(white)
+{
+    let img_x = 400
+    let img_y = white ? 0 : 200
+}
+
+var knight = function(white)
+{
+    let img_x = 600
+    let img_y = white ? 0 : 200
+}
+
+var rook = function(white)
+{
+    let img_x = 800
+    let img_y = white ? 0 : 200
+}
+
+var pawn = function(white)
+{
+    let img_x = 1000
+    let img_y = white ? 0 : 200
+}*/
